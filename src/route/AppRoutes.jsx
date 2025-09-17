b1bce5c05872d7529b6ee25e51b690b767eec8b5
RudraBajaj <rudrabajajdabwali@gmail.com>
2025-09-17 22:57:17 +0530

    Managing Routes

--- src/route/AppRoutes.jsx
+++ src/route/AppRoutes.jsx
@@ -10,8 +10,20 @@ const AppRoute = () => (
             {/* public */}
             <Route path="/" element={<LandingPage />} />
             <Route path="/login" element={<LoginPage />} />
-            <Route path="/login" element={<RegisterPage />} />
-            
+            <Route path="/register" element={<RegisterPage />} />
+            {/* Dashbaord  */}
+            <Route path="/alumni" element={<AlumniDashboard />} />
+            <Route path="/student" element={<StudentDashboard />} />
+            <Route path="/admin" element={<AdminDashboard />} />
+            {/* Features  */}
+            <Route path="/directory" element={<AlumniDashboard />} />
+            <Route path="/events" element={<Events />} />
+            <Route path="/donations" element={<Donations />} />
+            <Route path="/feedback" element={<Feedback />} />
+            {/* 404 Fallback  */}
+            <Route path="*" element={<NotFound />} />
         </Routes>
     </Router>
-)
+);
+
+export default AppRoute;
