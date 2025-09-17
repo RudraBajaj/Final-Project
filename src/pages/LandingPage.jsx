1a9c9b2174e8cfbba0955d19e12ae44b3c029e6e
RudraBajaj <rudrabajajdabwali@gmail.com>
2025-09-17 23:47:39 +0530

    Milimum App Routing done ,

---
+++ src/pages/LandingPage.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const LandingPage = () => (
+    <h1> Welcome to Our Landing Page</h1>
+)
+
+export default LandingPage;
--- src/route/AppRoutes.jsx
+++ src/route/AppRoutes.jsx
@@ -2,7 +2,20 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 // Page Imports 
-// import LandingPage from '../pages/LandingPage'
+import LandingPage from '../pages/LandingPage';
+import LoginPage from '../pages/LoginPage';
+import RegisterPage from '../pages/RegisterPage';
+
+//Missing pages 
+const AlumniDashboard = () => <div><h1>Alumni Dashboard Works! 🎓</h1></div>;
+const StudentDashboard = () => <div><h1>Student Dashboard Works! 🎒</h1></div>;
+const AdminDashboard = () => <div><h1>Admin Dashboard Works! ⚡</h1></div>;
+const AlumniDirectory = () => <div><h1>Alumni Directory Works! 📚</h1></div>;
+const Events = () => <div><h1>Events Works! 📅</h1></div>;
+const Donations = () => <div><h1>Donations Works! 💰</h1></div>;
+const Feedback = () => <div><h1>Feedback Works! 💬</h1></div>;
+const NotFound = () => <div><h1>404 - Page Not Found! 🚫</h1></div>;
+
 
 const AppRoute = () => (
     <Router>
